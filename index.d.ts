ae8175162eae874f44b4a4304c49fd930f38a42a
Will Dimmit <will@arioliving.com>
2018-04-25 09:24:32 -0700

    Typescript definition updates

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
+// Project: [~THE PROJECT NAME~]
+// Definitions by: [~YOUR NAME~] <[~A URL FOR YOU~]>
+
+export as namespace AndroidAESCCMModule;
+
+export function setKey(key: number[]): void;
+export function encrypt(aad: number[], clear: number[], nonce: number[]): Promise<number[]>;
+export function decrypt(aad: number[], encrypted: number[], nonce: number[], tag: number): Promise<number[]>;
