3d98e11854e0b8b968c2417a4cddc744a343b66c
Will Dimmit <will@arioliving.com>
2018-06-01 16:01:56 -0700

    Fix typescript def

--- index.d.ts
+++ index.d.ts
@@ -6,4 +6,4 @@ export as namespace AndroidAESCCMModule;
 
 export function setKey(key: number[]): void;
 export function encrypt(aad: number[], clear: number[], nonce: number[]): Promise<number[]>;
-export function decrypt(aad: number[], encrypted: number[], nonce: number[], tag: number): Promise<number[]>;
+export function decrypt(aad: number[], encrypted: number[], nonce: number[], tag: number[]): Promise<number[]>;
